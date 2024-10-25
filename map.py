d0d412954424b9e5aed052d9394cddce2b211a94
BetoHammerle <80226843+BetoHammerle@users.noreply.github.com>
2024-10-25 15:37:42 -0300

    Update map.py

--- map.py
+++ map.py
@@ -21,7 +21,7 @@ st.sidebar.info(
 
 st.sidebar.title("Registros de Campo")
 
-file = open ('D:\Biblioteca Python\mapa_host\cac.txt',"r",-1)
+file = open ('cac.txt',"r",-1)
       
 content = file.read()
 
